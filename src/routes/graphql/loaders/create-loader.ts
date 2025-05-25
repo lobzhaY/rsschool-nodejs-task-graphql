ce9020d503e763c53120f805430adc3a7567ff6b
Yuliya Lobzha <yulia.lobzha@gmail.com>
2025-05-25 14:24:41 +0300

    feat(loader): add subscribe loaders

--- src/routes/graphql/loaders/create-loader.ts
+++ src/routes/graphql/loaders/create-loader.ts
@@ -2,8 +2,11 @@ import { PrismaClient } from "@prisma/client";
 import DataLoader from "dataloader";
 import { getMemberLoaderData } from "./loader-utils/member-loader.js";
 import { getUserLoaderData } from "./loader-utils/user-loader.js";
+import { getSubscribedToUserLoader, getUserSubscribedToLoader } from "./loader-utils/subscribe-loader.js";
 
 export const createLoaders = (prisma: PrismaClient) => ({
     memberLoader: new DataLoader(getMemberLoaderData(prisma)),
     usersLoader: new DataLoader(getUserLoaderData(prisma)),
+    userSubscribedToLoader: getUserSubscribedToLoader(prisma),
+    subscribedToUserLoader: getSubscribedToUserLoader(prisma),
   }); 
